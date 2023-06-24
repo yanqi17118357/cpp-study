613c46a64b47d3e79925327b38ef0b2fd4b90af6
yanqi17118357 <3224848830@qq.com>
2023-06-24 16:26:19 +0800

    feat: conditions and branches

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include "Log.h"
 
 int main() {
+    int x = 5;
+    const char* ptr = nullptr;
+    // ptr是有效值，则输出ptr的值
+    if (ptr)
+        Log(ptr);
+    // 只有当前面的if语句为false时，才会执行else if语句
+    // else if just is else + if
+    // else {
+    //     if (ptr == "Hello")
+    //         Log("ptr is Hello");
+    // }
+    else if (ptr == "Hello")
+        Log("ptr is Hello");
+    else
+        Log("ptr is null!");
 
-    std::cout << "Hello, World!" << std::endl;
     std::cin.get();
 }
-
