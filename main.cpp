4c714c8ed70773128a3bb41299d94e0394c65926
yanqi17118357 <3224848830@qq.com>
2023-06-24 10:49:29 +0800

    feat: C++ Header Files

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << sizeof(bool) << std::endl; // 1 byte
-    std::cout << sizeof(char) << std::endl; // 1 byte
-    std::cout << sizeof(short) << std::endl; // 2 bytes
-    std::cout << sizeof(int) << std::endl; // 4 bytes
-    std::cout << sizeof(long) << std::endl; // 4 bytes
-    std::cout << sizeof(long long) << std::endl; // 8 bytes
-    std::cout << sizeof(float) << std::endl; // 4 bytes
-    std::cout << sizeof(double) << std::endl; // 8 bytes
-    std::cout << sizeof(long double) << std::endl; // 8 bytes
+
+    std::cout << "Hello, World!" << std::endl;
     std::cin.get();
 }
 
